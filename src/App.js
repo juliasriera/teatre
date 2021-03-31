a85e2cd6789e1147c3dd3cc3f0c8f064234a6041
juliasriera <juliasriera@gmail.com>
2021-03-31 16:21:27 +0200

    array

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 
 import './App.css';
 import Mylinia from './linia.js'
+import MyText from './text.js'
 
 function App() {
+
+  const textObra = MyText.map(item => <Mylinia item={item}/>)
+  
   return (
     <div className="obra">
       <header className="App-header">
-       <Mylinia text="Hola Romeo" />
-       <Mylinia text="Hola Julieta" />
-       <Mylinia text="T'estimo, Romeo"/>
-       <Mylinia text="I jo a tu Julieta"/>
-       <Mylinia text="Escapem-nos a Mèxic"/>
-       <Mylinia text="Millor no, no li caus bé a la meva família i si seguim endavant amb el nostre amor, aquesta història acabarà malament"/>
-       <Mylinia text="Doncs vaja"/>
+      {textObra}
       </header>
     </div>
   );
--- src/linia.js
+++ src/linia.js
@@ -1,9 +1,10 @@
 
+import React from 'react'
+
 function Mylinia(props){
-    console.log(props)
     return(
         <div className="linia-texto">
-            <p>{props.text}</p>
+            <p>{props.item}</p>
         </div>
     )
 }
